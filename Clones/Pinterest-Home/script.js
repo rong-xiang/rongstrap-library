0e1d80ce4b4e2cf3bf0fd6749e9e560a52cf288e
Rong Xiang <rongxiang19@gmail.com>
2021-02-28 01:17:03 -0500

    Fixed icon cursors. Finished dropdown JS.

--- Clones/Pinterest-Home/script.js
+++ Clones/Pinterest-Home/script.js
@@ -4,13 +4,14 @@
 
 To implement:
 
-- Nav dropdowns
-- Pin dropdowns
+- Nav dropdowns - done
+- Pin dropdowns - done
     - Cursor so that it's only zoom-in when on hover overlay but not on the dropdown content - done (fixed in CSs)
     - when you click, the search bar is already focused (ehh, optional)
-- Plus and question dropdowns
+    - Hover only triggers black background / opacity overlay, and elements
+    - If you click on one of overlay elements, the overlay stays on until you click off (like with dropdowns)
+- Plus and question dropdowns - done
 - On scroll, nav gets a shadow
-- 
 
 You can either:
 #1 add an event listener on the parent and use event delegation, OR
@@ -18,32 +19,32 @@ You can either:
 
 */
 
-// IMPLEMENT DROPDOWNS
+//////////* IMPLEMENT DROPDOWNS *//////////
 
 //////////* Using Method #2 *//////////
+
 const dropdowns = document.querySelectorAll('.dropdown');
 const dropdownContents = document.querySelectorAll('.dropdown-content');
 let target;
+
+function close() {
+  dropdownContents.forEach(content => content.classList.remove('show'));
+}
+
+function open(dropdownContent) {
+  close();
+  dropdownContent.classList.add('show'); // open
+}
 
 dropdowns.forEach(function (dropdown) {
   // For each dropdown, find its child dropdown-content and toggle the show class
   dropdown.addEventListener('click', function (e) {
     e.preventDefault();
     const dropdownContent = dropdown.querySelector('.dropdown-content');
-
-    // Toggle
-    if (dropdownContent.classList.contains('show')) {
-      close();
-    } else {
-      close();
-      dropdownContent.classList.add('show'); // open
-    }
-
+    dropdownContent.classList.contains('show')
+      ? close()
+      : open(dropdownContent); // toggle
     if (dropdownContent.classList.contains('show')) target = dropdown; // set target
-
-    function close() {
-      dropdownContents.forEach(content => content.classList.remove('show'));
-    }
   });
 });
 
@@ -52,9 +53,7 @@ window.addEventListener('click', function (e) {
   // check that you're clicking outside of the selected dropdown
   let selected = e.target.closest('.dropdown');
   if (!target?.contains(selected)) {
-    console.log(`target doesn't match!`);
     // remove the show class from the target's dropdown class
-    dropdownContents.forEach(content => content.classList.remove('show'));
-    console.log(target.querySelector('.dropdown-content').classList);
+    close();
   }
 });
